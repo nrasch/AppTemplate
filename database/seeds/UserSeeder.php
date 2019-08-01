d76dba50459c4c09d48faba2849f8e3cc4b47dec
Nathan Rasch <nathan.rasch@state.co.us>
2019-08-01 11:17:15 -0600

    Initial commit of PartOne

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+  /**
+  * Run the database seeds.
+  *
+  * @return void
+  */
+  public function run()
+  {
+    // Create an admin user
+    $user = User::create([
+      'name' => 'Admin',
+      'email' => 'admin@admin.com',
+      'password' => bcrypt('password')
+    ]);
+    $user->save();
+
+    // Create dev/test data for non-production environments
+    if (env('APP_ENV') != 'production') {
+      // Create N mumber of users
+      factory(User::class, 20)->make()->each(function($user) {
+        $user->save();
+        return true;
+      });
+    }
+  }
+}
