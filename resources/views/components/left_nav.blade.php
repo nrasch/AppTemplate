72cdbc3681cd2cebdca3dbcaae462645b0ab76d7
Nathan Rasch <nathan.rasch@state.co.us>
2019-08-02 10:14:30 -0600

    Adding Font Awesome assets

--- resources/views/components/left_nav.blade.php
+++ resources/views/components/left_nav.blade.php
@@ -1,11 +1,11 @@
 <div class="bg-light border-right" id="sidebar-wrapper">
   <div class="sidebar-heading">Start Bootstrap </div>
   <div class="list-group list-group-flush">
-    <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
-    <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
-    <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
-    <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
-    <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
-    <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fab fa-elementor mr-2"></i>Dashboard</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-external-link-alt mr-2"></i>Shortcuts</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-file-import mr-2"></i>Overview</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light"><i class="far fa-calendar-check mr-2"></i>Events</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-user mr-2"></i>Profile</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-info-circle mr-2"></i>Status</a>
   </div>
-</div>      
+</div>
